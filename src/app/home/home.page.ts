94b944b925f32bb7aed9f9972413c4b2edd68713
manoj997 <dharmakumar338@gmail.com>
2019-10-05 21:26:14 +0530

    code cleanup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AngularFirestore } from 'angularfire2/firestore';
-import{Storage} from '@ionic/storage';
-import{StafServiceService} from '../services/staf-service.service';
+import { Storage } from '@ionic/storage';
+import { StafServiceService } from '../services/staf-service.service';
 
 import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner/ngx';
 
@@ -14,70 +14,54 @@ import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-sca
 export class HomePage implements OnInit {
 
 
-  public name:any;
-  public user_email:any;
-
-
-
-
+  public name: any;
+  public user_email: any;
+  photoUrl:any;
+  showCard:boolean=false;
   barcodeScannerOptions: BarcodeScannerOptions;
-  uid:any;
+  uid: any;
   constructor(
-    public router:Router,
+    public router: Router,
     private firestore: AngularFirestore,
-    private storage:Storage,
-    private firebaseService:StafServiceService,
-    public barcodeScanner:BarcodeScanner
+    private storage: Storage,
+    private firebaseService: StafServiceService,
+    public barcodeScanner: BarcodeScanner
   ) {
-    this.barcodeScannerOptions={
-      showTorchButton:true,
-      showFlipCameraButton:true
-      
+    this.barcodeScannerOptions = {
+      showTorchButton: true,
+      showFlipCameraButton: true
+
     }
 
   }
-  
-ngOnInit(){
-  this.firebaseService.userChanges();
-    
-   // this.setTheValue();
-    this.getTheValue();
-}
 
-getTheValue(){
-    
-  this.storage.get("users").then( (val) =>{
-      if(val){ 
-        this.name=val.name;
-        
-          this.user_email=val.email;
-          //this.photo_URL=val.photoURL;
-         console.log(this.name);
-           }
-  })
+  ngOnInit() {
+    this.firebaseService.userChanges();
+  }
 
- }
-
-
-
-
-
-  scanCode(){
-    var x=true;
-    this.barcodeScanner.scan(this.barcodeScannerOptions).then(data=>{
-      this.uid=data;
-      this.firestore.collection("users").ref.where("id","==",data.text).onSnapshot(snap=>{
-        snap.forEach(ref=>{
-          if(x){
-            this.firestore.collection("users").doc(ref.id).update({isQrScanned:true});
-            x=false;
+ 
+  scanCode() {
+    var x = true;
+    this.barcodeScanner.scan(this.barcodeScannerOptions).then(data => {
+      this.uid = data;
+      this.firestore.collection("users").ref.where("id", "==", data.text).onSnapshot(snap => {
+        snap.forEach(ref => {
+          if (x) {
+            this.name=ref.data().name;
+            this.user_email=ref.data().email;
+            this.photoUrl=ref.data().photoURL;
+            this.showCard=true;
+            
+            this.firestore.collection("users").doc(ref.id).update({ isQrScanned: true });
+            x = false;
           }
-          
+
         })
       })
-      
-    }).catch(err =>{console.log(err);
+
+    }).catch(err => {
+      console.log(err);
     })
   }
- 
+
 }
